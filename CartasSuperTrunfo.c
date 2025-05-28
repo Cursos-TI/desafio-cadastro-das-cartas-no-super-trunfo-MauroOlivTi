719b097e9ac9ee622c02d5242dd8070ab57f01e5
MauroOlivTi <maurinhooliveiracastro@gmail.com>
2025-05-28 14:17:36 +0000

    Finalizacao Desafio Mestre Super Trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
@@ -23,10 +24,11 @@ float pib1 = 0, pib2 = 0;
 int pontostur1 = 0, pontostur2 = 0;
 float densidadepop1 = 0, densidadepop2 = 0;
 float pibcapita1 = 0, pibcapita2 = 0;
+float superpoder1 = 0, superpoder2 = 0;
 
 //Aqui começamos a digitar as informações da primeira carta
 
-printf("Cadastre os dados da primeira carta do jogo SuperTrunfo, lembre-se, sempre use ponto ao invés de vírgula nas informações que digitar.");
+ printf("Cadastre os dados da primeira carta do jogo SuperTrunfo, lembre-se, sempre use ponto ao invés de vírgula nas informações que digitar.");
 
 printf("\n\nDigite a letra que represente o Estado da primeira carta, use letras maiúsculas de 'A' à 'H' para representá-lo: "); 
 scanf(" %c", &estado1);
@@ -84,20 +86,49 @@ densidadepop2 = populacao2 / area2;
 pibcapita1 = pib1 / populacao1; //como pib ja esta declarado em float nao precisaremos do casting
 pibcapita2 = pib2 / populacao2;
 
+//Como temos que tomar cuidado apenas na divisao com os tipos de variaveis, vamos apenas somar:
+
+superpoder1 = populacao1 + area1 + pib1 + pontostur1 + pibcapita1 + 1/densidadepop1;
+superpoder2 = populacao2 + area2 + pib2 + pontostur2 + pibcapita2 + 1/densidadepop2;
+
 //Abaixo o programa mostrara os dados das duas cartas
+//Adicionar o Status SuperPoder as cartas
 
 printf("\nConfira abaixo os dados da primeira carta cadastrada: ");
 
-printf("\n\n Estado: %c \n Código da carta: %s \n Nome da cidade: %s \n População: %d habitantes \n Área: %.2f km² \n PIB: %.2f reais\n Número de pontos turisticos: %d\n Densidade Populacional: %.2f habitantes/km²\n PIB per Capita: %.2f reais/habitante", estado1, codigo1, cidade1, populacao1, area1, pib1, pontostur1, densidadepop1, pibcapita1 );
+printf("\n\n Estado: %c \n Código da carta: %s \n Nome da cidade: %s \n População: %d habitantes \n Área: %.2f km² \n PIB: %.2f reais\n Número de pontos turisticos: %d\n Densidade Populacional: %.2f habitantes/km²\n PIB per Capita: %.2f reais/habitante\nSuper Poder: %.2f", estado1, codigo1, cidade1, populacao1, area1, pib1, pontostur1, densidadepop1, pibcapita1, superpoder1 );
 
 printf("\n\nConfira abaixo os dados da segunda carta cadastrada:");
 
-printf("\n\n Estado: %c \n Código da carta: %s \n Nome da cidade: %s \n População: %d habitantes \n Área: %.2f km² \n PIB: %.2f reais\n Número de pontos turisticos: %d\n Densidade Populacional: %.2f habitantes/km²\n PIB per Capita: %.2f reais/habitante", estado2, codigo2, cidade2, populacao2, area2, pib2, pontostur2, densidadepop2, pibcapita2 );
+printf("\n\n Estado: %c \n Código da carta: %s \n Nome da cidade: %s \n População: %d habitantes \n Área: %.2f km² \n PIB: %.2f reais\n Número de pontos turisticos: %d\n Densidade Populacional: %.2f habitantes/km²\n PIB per Capita: %.2f reais/habitante\nSuper Poder: %.2f", estado2, codigo2, cidade2, populacao2, area2, pib2, pontostur2, densidadepop2, pibcapita2, superpoder2 );
+
+//Aqui compararemos os valores de cada carta para ver qual vence em cada situacao
+//Usei a funcao pow e a library math.h para conseguir mostrar qual carta venceu sem usar funcoes de decisao
+
+printf("\n\nA seguir compararemos os valores de cada carta para ver qual venceu: ");
+
+//Por alguma razao o codigo abaixo funciona no vscode mas nao funciona aqui no github
+
+/*printf("\n\n População: Carta %d venceu", (int) pow(2,populacao2 > populacao1));
+printf("\n Área: Carta %d venceu", (int) pow(2, area2 > area1));
+printf("\n PIB: Carta %d venceu", (int) pow(2, pib2 > pib1));
+printf("\n Pontos Turísticos: Carta %d venceu", (int) pow(2, pontostur2 > pontostur1));
+printf("\n Densidade Populacional: Carta %d venceu", (int) pow(2, densidadepop2 < densidadepop1));
+printf("\n PIB per Capita: Carta %d venceu", (int) pow(2, pibcapita2 > pibcapita1));
+printf("\n Super Poder: Carta %d venceu", (int) pow(2, superpoder2 > superpoder1));
+*/
+
+printf("\n\n População: Carta 1 venceu(%d) ou Carta 2 venceu(%d)", populacao2 < populacao1, populacao2 > populacao1);
+printf("\n Área: Carta 1 venceu(%d) ou Carta 2 venceu(%d)", area2 < area1, area2 > area1);
+printf("\n PIB: Carta 1 venceu(%d) ou Carta 2 venceu(%d)", pib2 < pib1, pib2 > pib1);
+printf("\n Pontos Turísticos: Carta 1 venceu(%d) ou Carta 2 venceu(%d)", pontostur2 < pontostur1, pontostur2 > pontostur1);
+printf("\n Densidade Populacional: Carta 1 venceu(%d) ou Carta 2 venceu(%d)",densidadepop2 > densidadepop1, densidadepop2 < densidadepop1);
+printf("\n PIB per Capita: Carta 1 venceu(%d) ou Carta 2 venceu(%d)", pibcapita2 < pibcapita1, pibcapita2 > pibcapita1);
+printf("\n Super Poder: Carta 1 venceu(%d) ou Carta 2 venceu(%d)", superpoder2 < superpoder1, superpoder2 > superpoder1);
+
+printf("\n\nCaso os valores das suas cartas tenham dado 0, significa um empate.\n");
 
 return 0;
 
 
 }
-
-
-
